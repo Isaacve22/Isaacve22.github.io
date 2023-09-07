85b2c48f33dd5b0e8e180280d52cfd2e2be8bb63
Isaacve22 <isaacvelilla22@gmail.com>
2023-09-07 02:38:29 -0400

    Añadí imágenes y cambié los estilos css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fecha de finalización de la cuenta regresiva
-const endDate = new Date('2023-07-29T15:19:00').getTime();
+const endDate = new Date('2022-09-07T00:00:00').getTime();
 
 function updateCountdown() {
   const now = new Date().getTime();
@@ -7,9 +7,16 @@ function updateCountdown() {
 
   if (timeLeft <= 0) {
     // Mostrar el vídeo y el texto adicional cuando la cuenta regresiva ha terminado
-    document.getElementById('countdown').style.display = 'none';
+    
+    document.getElementById('title').style.display = 'none'; // Ocultar el título "Faltan"
+    document.getElementById('countdown').style.display = 'none'
     document.getElementById('video-container').style.display = 'block';
-    document.getElementById('text-container').style.display = 'block';
+    document.querySelectorAll('.aftercountdown').forEach(element => {
+      element.style.display = 'block';
+    });
+    
+    
+    
   } else {
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
